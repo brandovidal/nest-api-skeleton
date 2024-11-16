d43ab496384c0c28f7ba615b0533122f00c43ca5
Brando Vidal <yisusdracon@gmail.com>
2024-11-16 00:41:27 -0500

    feat: customize errors validation

--- src/models/user/dto/create-user.dto.ts
+++ src/models/user/dto/create-user.dto.ts
@@ -3,9 +3,18 @@ import { createZodDto } from 'nestjs-zod'
 import { object, string } from 'zod'
 
 const createSchema = object({
-  username: string({ required_error: 'Username is required' }),
-  email: string(),
-  password: string(),
+  username: string({ required_error: 'Enter your username.', invalid_type_error: 'Validate your username.' })
+    .trim()
+    .min(3, { message: 'Enter 3 characters as minimum.' }),
+  email: string({ required_error: 'Enter your email.', invalid_type_error: 'Validate your email.' })
+    .trim()
+    .min(5, { message: 'Enter 5 characters as minimum.' })
+    .email({
+      message: 'Enter a valid email.'
+    }),
+  password: string({ required_error: 'Enter your password.', invalid_type_error: 'Validate your password.' })
+    .trim()
+    .min(6, { message: 'Enter 6 characters as minimum.' })
 }).required()
 
 export class CreateUserDto extends createZodDto(createSchema) {}
