5e7d91e608985448ed0d3aacbfc22838600f7b73
Brando Vidal <yisusdracon@gmail.com>
2024-11-17 02:41:08 -0500

    fix: ignore vitest config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   env: {
     node: true
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'vitest.workspace.mts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
