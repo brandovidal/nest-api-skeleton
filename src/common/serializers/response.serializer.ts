61bd1e6de4de66486538ea0953153f305b10f2fd
Brando Vidal <yisusdracon@gmail.com>
2024-11-17 04:04:19 -0500

    feat: format response serializer

--- src/common/serializers/response.serializer.ts
+++ src/common/serializers/response.serializer.ts
@@ -1,6 +1,7 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpException, HttpStatus, Logger } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { ZodSerializationException } from 'nestjs-zod'
+
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 
 import { Observable, throwError } from 'rxjs'
@@ -29,11 +30,11 @@ export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
   intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
     return next.handle().pipe(
       map((res: unknown) => this.responseHandler(res, context)),
-      catchError((err: HttpException) => throwError(() => this.errorHandler(err, context)))
+      catchError((err) => throwError(() => this.errorHandler(err, context)))
     )
   }
 
-  errorHandler(exception: HttpException, context: ExecutionContext) {
+  errorHandler(exception, context: ExecutionContext) {
     const ctx = context.switchToHttp()
 
     const response = ctx.getResponse()
@@ -41,28 +42,29 @@ export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
 
     const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR
 
-    let errors
+    let errors = exception
     if (exception.name === 'ZodValidationException') {
       const zodError = (exception as ZodSerializationException).getZodError()
       errors = zodError.issues ?? zodError.errors
     } else if (exception.name === 'PrismaClientKnownRequestError') {
       const prismaError = exception as unknown as PrismaClientKnownRequestError
-      errors = prismaError?.meta ?? prismaError.code
+      errors = { code: prismaError.code, message: prismaError.meta }
     } else if (exception instanceof HttpException) {
       errors = exception.message
     } else {
-      errors = exception
+      this.logger.error(exception.stack)
     }
 
+    const { url: path, method, body, query, params } = request
     const result = {
       success: false,
       status,
-      path: request.url,
+      path,
       message: exception.message,
       timestamp: dayjs(new Date().toISOString()).format('YYYY-MM-DD HH:mm:ss'),
       errors: errors
     }
-    this.logger.log({ ...result, request: request.body ?? request.query ?? request.params })
+    this.logger.log({ ...result, method, request: { body, query, params } })
 
     response.status(status).json(result)
   }
@@ -76,15 +78,17 @@ export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
     const status = response.statusCode
     const message = this.reflector.get<string>(RESPONSE_MESSAGE_METADATA, context.getHandler()) || 'success'
 
+    const { url: path, method, body, query, params } = request
     const result = {
       success: true,
-      path: request.url,
+      path,
       message: message,
       status,
       data: res,
       timestamp: dayjs(new Date().toISOString()).format('YYYY-MM-DD HH:mm:ss')
     }
-    this.logger.log({ ...result, request: request.body ?? request.query ?? request.params })
+    this.logger.log({ ...result, method, request: { body, query, params } })
+
     return result
   }
 }
