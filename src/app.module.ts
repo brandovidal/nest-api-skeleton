b84a3634502df6242334c6a1bd5dd1181cc1ee94
Brando Vidal <yisusdracon@gmail.com>
2024-11-18 00:44:10 -0500

    refactor: create auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { LoggerModule } from './providers/logger/logger.module'
 import { PostgresProviderModule } from './providers/database/postgres/provider.module'
 
 import { UserModule } from './models/user/user.module'
+import { AuthModule } from './models/auth/auth.module'
 
 import { AppController } from './app.controller'
 
 import { AppService } from './app.service'
 
 @Module({
-  imports: [LoggerModule, PostgresProviderModule, UserModule],
+  imports: [LoggerModule, PostgresProviderModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
